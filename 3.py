6f84b8426eb635637e962fe59106695e28bddbf7
Mithilesh Gowda HA <mithilesh.gwda@gmail.com>
2026-06-29 08:10:50 +0530

    Implement basic operations and mini project in 3.py
    Added a mini project for input and output, including name and age difference calculation.

---
+++ 3.py
@@ -0,0 +1,30 @@
+#assinging values
+
+a = 10
+print(a)
+
+#adding 2 numbers
+
+a = 10
+b = 20
+print(a+b) #addading two values
+
+
+a = 10
+b = 20
+
+temp = a
+a = b
+b = temp
+
+print(a,b)  #swapping 2 values
+
+# MINI PROJECT UAING INPUT AND OUTPUT
+
+boy_name = input("Enter boy name :")
+boy_age = int(input("Enter boy age :"))
+girl_name = input("Enter girl name :")
+girl_age = int(input("Enter girl name :"))
+
+age_diff = boy_age - girl_age
+print(boy_name + " loves " + girl_name + ".Age difference is" + str(age_diff) )
