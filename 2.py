2abc525bcf8082b77b7d9dfec7e0b66f6b072038
Mithilesh Gowda HA <mithilesh.gwda@gmail.com>
2026-06-29 08:09:37 +0530

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,1 +1,10 @@
-bvfhv
+name = "Mithilesh" #string
+age = 18 #int
+weight = 53.54 #float
+is_student = "True" #bool
+
+print(age)
+
+print(type(weight)) #to check the type of data types
+
+print(float(age)) 
